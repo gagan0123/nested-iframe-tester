ab600a0ecbdc38e1b9c2f123dbfde4c6d6818b52
Gagan Deep Singh <gagandeep.singh@rtcamp.com>
2024-04-12 18:23:11 +0530

    Fixes for domain paths and names

--- server.js
+++ server.js
@@ -10,86 +10,71 @@ app.use((req, res, next) => {
 });
 
 // Function to generate cookie setting string
-const setCookieString = (name, value) => `Set-Cookie: ${name}=${value}; Path=/; HttpOnly; SameSite=None; Secure`;
+const setCookieString = (name, value) => `${name}=${value}; Path=/; HttpOnly; SameSite=None; Secure`;
 
-// Single Level Nesting - domain1.com
-app.get('/domain1.com', (req, res) => {
-	const cookieString = setCookieString('SingleLevelCookie', 'domain1');
+// Domain 1 - Top Level
+app.get('/', (req, res) => {
+	// Assuming domain1.com is the default entry, no need to specify /domain1.com in the route
+	const cookieString = setCookieString(req.hostname+'TopLevelCookie', req.hostname);
 	res.setHeader('Set-Cookie', cookieString);
 	res.send(`
-    <h1>Domain 1 - Top Level</h1>
-    <iframe src="https://domain2.com/domain2.com/single" width="300" height="200"></iframe>
+    <h1>`+req.hostname+` - Top Level</h1>
+    <iframe src="https://domain2.com/single" width="300" height="200"></iframe>
     `);
 });
 
-// Domain 2 route for single level nesting
-app.get('/domain2.com/single', (req, res) => {
-	const cookieString = setCookieString('SingleLevelCookie', 'domain2Single');
-	res.setHeader('Set-Cookie', cookieString);
-	res.send('<h1>Domain 2 - Single Nested Iframe</h1>');
-});
+// Define routes for domain2.com
+app.route('/single')
+   .get((req, res) => {
+	   const cookieString = setCookieString(req.hostname+'SingleLevelCookie', req.hostname+'Single');
+	   res.setHeader('Set-Cookie', cookieString);
+	   res.send('<h1>Domain 2 - Single Nested Iframe</h1>');
+   });
 
-// Multi-level Nesting - starting at domain2.com/multi
-app.get('/domain2.com/multi', (req, res) => {
-	const cookieString = setCookieString('MultiLevelCookie', 'domain2Multi');
-	res.setHeader('Set-Cookie', cookieString);
-	res.send(`
-    <h1>Domain 2 - Multi Level</h1>
-    <iframe src="https://domain3.com/domain3.com/multi" width="300" height="200"></iframe>
-    `);
-});
+app.route('/multi')
+   .get((req, res) => {
+	   const cookieString = setCookieString(req.hostname+'MultiLevelCookie', req.hostname+'Multi');
+	   res.setHeader('Set-Cookie', cookieString);
+	   res.send(`
+        <h1>`+req.hostname+` - Multi Level</h1>
+        <iframe src="https://domain3.com/multi" width="300" height="200"></iframe>
+        `);
+   });
 
-// Domain 3 route for multi-level nesting
-app.get('/domain3.com/multi', (req, res) => {
-	const cookieString = setCookieString('MultiLevelCookie', 'domain3Multi');
-	res.setHeader('Set-Cookie', cookieString);
-	res.send(`
-    <h1>Domain 3 - Nested inside Domain 2</h1>
-    <iframe src="https://domain4.com/domain4.com" width="300" height="200"></iframe>
-    `);
-});
+app.route('/sibling')
+   .get((req, res) => {
+	   const cookieString = setCookieString(req.hostname+'SiblingCookie', req.hostname+'Sibling');
+	   res.setHeader('Set-Cookie', cookieString);
+	   res.send(`
+        <h1>`+req.hostname+` - Sibling Iframes</h1>
+        <iframe src="https://domain3.com/" width="300" height="200"></iframe>
+        <iframe src="https://domain4.com/" width="300" height="200"></iframe>
+        `);
+   });
 
-// Sibling Iframes - domain2.com/sibling
-app.get('/domain2.com/sibling', (req, res) => {
-	const cookieString = setCookieString('SiblingCookie', 'domain2Sibling');
-	res.setHeader('Set-Cookie', cookieString);
-	res.send(`
-    <h1>Domain 2 - Sibling Iframes</h1>
-    <iframe src="https://domain3.com/domain3.com" width="300" height="200"></iframe>
-    <iframe src="https://domain4.com/domain4.com" width="300" height="200"></iframe>
-    `);
-});
+app.route('/mixed')
+   .get((req, res) => {
+	   const cookieString = setCookieString(req.hostname+'MixedCookie', req.hostname+'Mixed');
+	   res.setHeader('Set-Cookie', cookieString);
+	   res.send(`
+        <h1>`+req.hostname+` - Mixed Nesting</h1>
+        <iframe src="https://domain3.com/mixed" width="300" height="200"></iframe>
+        <iframe src="https://domain5.com/" width="300" height="200"></iframe>
+        `);
+   });
 
-// Mixed Nesting - domain2.com/mixed
-app.get('/domain2.com/mixed', (req, res) => {
-	const cookieString = setCookieString('MixedCookie', 'domain2Mixed');
-	res.setHeader('Set-Cookie', cookieString);
-	res.send(`
-    <h1>Domain 2 - Mixed Nesting</h1>
-    <iframe src="https://domain3.com/domain3.com/mixed" width="300" height="200"></iframe>
-    <iframe src="https://domain5.com/domain5.com" width="300" height="200"></iframe>
-    `);
-});
-
-// Domain 3 route for mixed nesting
-app.get('/domain3.com/mixed', (req, res) => {
-	const cookieString = setCookieString('MixedCookie', 'domain3Mixed');
-	res.setHeader('Set-Cookie', cookieString);
-	res.send(`
-    <h1>Domain 3 - Mixed Nesting</h1>
-    <iframe src="https://domain4.com/domain4.com" width="300" height="200"></iframe>
-    <iframe src="https://domain6.com/domain6.com" width="300" height="200"></iframe>
-    `);
-});
-
-// Simple routes for domains 4, 5, 6... with cookies
-['domain4.com', 'domain5.com', 'domain6.com'].forEach(domain => {
-	app.get(`/${domain}`, (req, res) => {
-		const cookieString = setCookieString(`${domain}Cookie`, `${domain}`);
-		res.setHeader('Set-Cookie', cookieString);
-		res.send(`<h1>${domain.toUpperCase()}</h1>`);
-	});
-});
+// Similar route structures for domain3.com, domain4.com, etc.
+// Example for domain3.com
+app.route('/multi')
+   .get((req, res) => {
+	   // Note: This assumes you're handling domain distinction elsewhere, as this conflicts with domain2's /multi
+	   const cookieString = setCookieString(req.hostname+'MultiLevelCookie', req.hostname+'Multi');
+	   res.setHeader('Set-Cookie', cookieString);
+	   res.send(`
+        <h1>`+req.hostname+` - Multi Level Nested</h1>
+        <iframe src="https://domain4.com/" width="300" height="200"></iframe>
+        `);
+   });
 
 // Start the server
 app.listen(port, () => {
